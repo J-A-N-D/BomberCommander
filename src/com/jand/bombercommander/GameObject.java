9ed14287b3afdb66750e214a5a7347a3b8c13455
Aleksandar Arsovski <alek_ars@hotmail.com>
2013-02-09 15:54:03 -0800

    Adds actual constructors to Player and GameObject

--- src/com/jand/bombercommander/GameObject.java
+++ src/com/jand/bombercommander/GameObject.java
@@ -5,6 +5,16 @@ public class GameObject {
 	protected boolean is_player_one;
 	protected int field_position;
 	
+	public GameObject() {
+		this.is_player_one = true;
+		this.field_position = 0;
+	}
+	
+	public GameObject(boolean start_is_player_one, int start_field_position) {
+		this.is_player_one = start_is_player_one;
+		this.field_position = start_field_position;
+	}
+	
 	
 	public boolean getIsPlayerOne() {
 		return is_player_one;
--- src/com/jand/bombercommander/Player.java
+++ src/com/jand/bombercommander/Player.java
@@ -2,10 +2,26 @@ package com.jand.bombercommander;
 
 public class Player {
 	
+	private static final int MAX_HP = 200;
+	private static final int MAX_BOMBS = 20;
+	
 	private int base_hp;
 	private int bombs_left;
 	//private list of live AA
 	private boolean fighter_has_kill;
+	
+	
+	public Player() {
+		this.base_hp = MAX_HP;
+		this.bombs_left = MAX_BOMBS;
+		this.fighter_has_kill = false;
+	}
+	
+	public Player(int start_base_hp, int start_bombs_left, boolean start_fighter_has_kill) {
+		this.base_hp = start_base_hp;
+		this.bombs_left = start_bombs_left;
+		this.fighter_has_kill = start_fighter_has_kill;
+	}
 	
 	
 	public int getBaseHP() {
