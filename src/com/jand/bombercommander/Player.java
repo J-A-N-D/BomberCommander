af1c192037a96933139ad1f2f614a0cc650a156b
Aleksandar Arsovski <alek_ars@hotmail.com>
2013-02-09 15:02:19 -0800

    Adding player class
    Player class is cool

---
+++ src/com/jand/bombercommander/Player.java
@@ -0,0 +1,36 @@
+package com.jand.bombercommander;
+
+public class Player {
+	
+	private int base_hp;
+	private int bombs_left;
+	//private list of live AA
+	private boolean fighter_has_kill;
+	
+	
+	public int getBaseHP() {
+		return base_hp;
+	}
+	
+	public int getBombsLeft() {
+		return bombs_left;
+	}
+	
+	public boolean getFighterHasKill() {
+		return fighter_has_kill;
+	}
+	
+	
+	public void setBaseHP(int new_base_hp) {
+		base_hp = new_base_hp;
+	}
+	
+	public void setBombsLeft(int updated_bombs_left) {
+		bombs_left = updated_bombs_left;
+	}
+	
+	public void setFighterHasKill(boolean updated_fighter_has_kill) {
+		fighter_has_kill = updated_fighter_has_kill;
+	}
+	
+}
