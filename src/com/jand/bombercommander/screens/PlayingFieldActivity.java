5da0f99698ee8859c8fb26ea593a6c2bb3b8dafd
Daniel McKerricher <dmckerricher@hotmail.com>
2013-02-09 18:58:51 -0800

    modified playingfieldactivity

--- src/com/jand/bombercommander/screens/PlayingFieldActivity.java
+++ src/com/jand/bombercommander/screens/PlayingFieldActivity.java
@@ -7,13 +7,27 @@ import com.jand.bombercommander.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
+import android.content.res.Resources;
+import android.widget.RelativeLayout;
+import android.graphics.drawable.NinePatchDrawable;
 
 public class PlayingFieldActivity extends Activity {
-
+	Paint paint;
+	Canvas canvas;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playing_field);
+		Bitmap b = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+		canvas = new Canvas(b);
+		
 	}
 
 	@Override
@@ -22,5 +36,11 @@ public class PlayingFieldActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_playing_field, menu);
 		return true;
 	}
-
+	
+	public void draw(){
+		//getCurrentFocus().draw(canvas);
+		Resources res = getResources();
+	    Drawable myImage = res.getDrawable(R.drawable.bc_aa);
+	    myImage.draw(canvas);
+	}
 }
