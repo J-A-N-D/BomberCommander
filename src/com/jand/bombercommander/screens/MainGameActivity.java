8b603954d9fac511f18e08ff445688bb668ab4b6
Johnathan Chu <jtychu@outlook.com>
2013-02-20 13:31:54 -0800

    More refactoring.

--- src/com/jand/bombercommander/screens/MainGameActivity.java
+++ src/com/jand/bombercommander/screens/MainGameActivity.java
@@ -2,19 +2,12 @@ package com.jand.bombercommander.screens;
 
 import com.jand.bombercommander.GameThread;
 import com.jand.bombercommander.R;
-import com.jand.bombercommander.R.layout;
 
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.content.Intent;
 
 public class MainGameActivity extends Activity {
 	
@@ -27,7 +20,7 @@ public class MainGameActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_player1_setup);
+		setContentView(R.layout.activity_main_game);
 		
 		playerText = (TextView)findViewById(R.id.textPlayerSetup);
 		actionButton = (Button)findViewById(R.id.btnAction);
@@ -52,37 +45,5 @@ public class MainGameActivity extends Activity {
 				}
 			}
 		});
-	}
-
-	@Override
-	public void onBackPressed() {
-		ExitDialogFragment dialog = new ExitDialogFragment();
-		dialog.show(getFragmentManager(), "loggingOut");
-	}
-	
-	public class ExitDialogFragment extends DialogFragment {
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			// Use the Builder class for convenient dialog construction
-			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-			builder.setMessage("Do you want to return to the main menu?")
-					.setPositiveButton("Yes",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									thread.setRunning(false);
-									finish();
-								}
-							})
-					.setNegativeButton("No",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									// Do nothing...
-								}
-							});
-			// Create the AlertDialog object and return it
-			return builder.create();
-		}
 	}
 }
--- src/com/jand/bombercommander/screens/StartScreenActivity.java
+++ src/com/jand/bombercommander/screens/StartScreenActivity.java
@@ -5,18 +5,12 @@
 package com.jand.bombercommander.screens;
 
 import com.jand.bombercommander.R;
-import com.jand.bombercommander.R.layout;
-import com.jand.bombercommander.screens.MainGameActivity.ExitDialogFragment;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.Intent;
 
 public class StartScreenActivity extends Activity {
@@ -25,7 +19,7 @@ public class StartScreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_screen);
+		setContentView(R.layout.activity_start_screen);
 		
 		Button startButton = (Button)findViewById(R.id.btnStart);
 		
